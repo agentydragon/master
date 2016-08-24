9715ed9defbc19dca437c07a7f0d7e47c5e7cd76
Michal Pokorný <pok@rny.cz>
2016-08-24 16:07:43 +0200

    Port spotlight translation

--- code/hadoop/AnnotateCoreferences.java
+++ code/hadoop/AnnotateCoreferences.java
@@ -1,15 +1,31 @@
 import java.util.List;
+import java.util.ArrayList;
+import org.json.*;
 
 public class AnnotateCoreferences {
-	public static class SpotlightMention {
-		// TODO
+	public List<Sentence.SpotlightMention> SpotlightToMentions(JSONObject spotlightJson) {
+		List<Sentence.SpotlightMention> mentions = new ArrayList<>();
+
+		JSONArray mentionsList = (JSONArray) spotlightJson.get("Resources");
+		for (Object o : mentionsList) {
+			JSONObject mentionJson = (JSONObject) o;
+			Sentence.SpotlightMention.Builder mentionBuilder = Sentence.SpotlightMention.newBuilder()
+				.setStartOffset(Integer.parseInt((String) mentionJson.get("@offset")));
+			if (mentionJson.get("@surfaceForm") == null) {
+				// TODO HACK?
+				continue;
+			}
+			String surfaceForm = (String) mentionJson.get("@surfaceForm");
+			mentionBuilder.setEndOffset(mentionBuilder.getStartOffset() + surfaceForm.length())
+				.setSurfaceForm(surfaceForm)
+				.setUri((String) mentionJson.get("@URI"));
+			mentions.add(mentionBuilder.build());
+		}
+
+		return mentions;
 	}
 
-	public List<SpotlightMention> SpotlightToMentions(JSONObject spotlightJson) {
-		return null;
-	}
-
-	public Sentence.Document PropagateEntities(Sentence.Document documentProto, List<SpotlightMention> spotlightMentions) {
+	public Sentence.Document PropagateEntities(Sentence.Document documentProto, List<Sentence.SpotlightMention> spotlightMentions) {
 		return null;
 	}
 }
