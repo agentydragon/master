31e3f40765e2818e619553ed9cbcb2ea2cf32444
Michal Pokorný <pok@rny.cz>
2016-09-06 14:42:55 +0200

    Remove run.sh

--- code/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-module add jdk-8
-module add python34-modules-gcc
-cd /storage/brno7-cerit/home/prvak/master/code
-/storage/brno7-cerit/home/prvak/bin/bazel run --script_path $SCRATCHDIR/script.sh spotlight:Spotlight
-source $SCRATCHDIR/script.sh
