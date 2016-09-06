fb73518cc08074f997c2f55b452136a688dfef72
Michal Pokorný <pok@rny.cz>
2016-09-06 12:07:11 +0200

    remove printing of jobscript
    stuff

--- code/py/pbs_util.py
+++ code/py/pbs_util.py
@@ -1,14 +1,7 @@
 import subprocess
 import sys
 
-def launch(walltime, node_spec, job_name, script):
-    qsub_command = ['qsub',
-                    '-l', 'walltime=' + walltime,
-                    '-l', node_spec,
-                    '-m', 'abe',
-                    '-N', job_name]
-    print(qsub_command)
-    job_script = ("""
+JOBSCRIPT_HEADER = """
 #!/bin/bash
 
 # TODO HAX
@@ -33,21 +26,29 @@ module add jdk-8
 module add python34-modules-gcc
 
 cd $PBS_O_WORKDIR
-""" + script)
+"""
+
+def launch(walltime, node_spec, job_name, script):
+    qsub_command = ['qsub',
+                    '-l', 'walltime=' + walltime,
+                    '-l', node_spec,
+                    '-m', 'abe',
+                    '-N', job_name]
+    print(qsub_command)
+    job_script = (JOBSCRIPT_HEADER + script)
 
     js_path = job_name + '.sh'
     with open(js_path, 'w') as jobscript_file:
         jobscript_file.write(job_script)
-    print(job_script)
+    #print(job_script)
 
     qsub_command.append(js_path)
     popen = subprocess.Popen(qsub_command,
-                             #stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
-    stdoutdata, stderrdata = popen.communicate()#job_script)
-    print(stdoutdata)
-    print(stderrdata)
+    stdoutdata, stderrdata = popen.communicate()
+    print('stdout:', stdoutdata)
+    print('stderr:', stderrdata)
     if popen.returncode != 0:
         print(popen.returncode)
         sys.exit(1)
