0e8158f054a9475fd037bed6178009e73436639d
Michal Pokorný <pok@rny.cz>
2016-09-07 16:12:25 +0200

    single-thread launch

--- code/prototype/add_parses/launch.py
+++ code/prototype/add_parses/launch.py
@@ -1,5 +1,6 @@
 from py import pbs_util
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser(description='TODO')
 parser.add_argument('--article_list_file', type=str, required=True)
@@ -12,14 +13,19 @@ with open(args.article_list_file) as f:
 if args.max_articles:
     article_names = article_names[:args.max_articles]
 
+# fails to run Java on remote machines :(
+
 job_command = [
     'prototype/add_parses/add_parses',
 ] + article_names
-job_id = pbs_util.launch_job(
-    # TODO: calculate walltime; parallelize
-    walltime="01:00:00",
-    node_spec="nodes=1:brno:ppn=1,mem=8gb",
-    job_name="add-parses",
-    job_command=job_command
-)
-print("Launched add-parses:", job_id)
+
+subprocess.check_output(job_command)
+
+#job_id = pbs_util.launch_job(
+#    # TODO: calculate walltime; parallelize
+#    walltime="01:00:00",
+#    node_spec="nodes=1:brno:ppn=1,mem=8gb",
+#    job_name="add-parses",
+#    job_command=job_command
+#)
+#print("Launched add-parses:", job_id)
