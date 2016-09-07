749fa7c7a8cfddc350dc0084965b3e75c93bf994
Michal Pokorný <pok@rny.cz>
2016-09-07 13:29:57 +0200

    refactor

--- code/prototype/add_parses/AddParses.java
+++ code/prototype/add_parses/AddParses.java
@@ -23,28 +23,31 @@ public class AddParses {
 		String sub = prefix(title, 1) + "/" + prefix(title, 2) + "/" + prefix(title, 3) + "/" + title + ".json";
 		return basePath + "/" + sub;
 	}
+	private static void processArticle(String title) {
+		try {
+			String path = articleTitleToPath(title);
+			System.out.println(path);
+
+			File file = new File(path);
+
+			String jsonContent = FileUtils.readFileToString(file);
+			JSONObject json = new JSONObject(jsonContent);
+			String articleText = (String) json.get("plaintext");
+			json.put("corenlp_xml", corenlpInterface.getXML(articleText));
+
+			FileUtils.writeStringToFile(file, json.toString());
+		} catch (IOException e) {
+			System.out.println("Failed: " + title);
+			System.out.println(e);
+		}
+	}
 
 	public static void main(String[] args) {
 		CoreNLPInterface corenlpInterface = new CoreNLPInterface();
 		corenlpInterface.setup();
 
 		for (String title : args) {
-			try {
-				String path = articleTitleToPath(title);
-				System.out.println(path);
-
-				File file = new File(path);
-
-				String jsonContent = FileUtils.readFileToString(file);
-				JSONObject json = new JSONObject(jsonContent);
-				String articleText = (String) json.get("plaintext");
-				json.put("corenlp_xml", corenlpInterface.getXML(articleText));
-
-				FileUtils.writeStringToFile(file, json.toString());
-			} catch (IOException e) {
-				System.out.println("Failed: " + title);
-				System.out.println(e);
-			}
+			processArticle(title);
 		}
 	}
 }
--- code/prototype/find_persons.py
+++ code/prototype/find_persons.py
@@ -7,15 +7,17 @@ from py import wikidata
 from py import wikidata_util
 import sys
 
+NUM_PERSONS = 10000
+
 wikidata_client = wikidata.WikidataClient()
 wikidata_client.persist_caches = False
 # ?person (instance of) (human)
-print("Looking for persons...")
+print("Looking for %d persons..." % NUM_PERSONS)
 results = wikidata_client.wikidata_client.get_results("""
     SELECT ?person
     WHERE { ?person wdp:P31 wd:Q5 }
-    LIMIT 1000
-""")
+    LIMIT %d
+""" % NUM_PERSONS)
 results = results['results']['bindings']
 for row in results:
     id = wikidata_util.wikidata_entity_url_to_entity_id(row['person']['value'])
