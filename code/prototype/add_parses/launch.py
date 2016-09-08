1cbdef7e8a7e0135125655bbcdbb1a11cfa96ee6
Michal Pokorný <pok@rny.cz>
2016-09-08 14:02:35 +0200

    add parallelization

--- code/prototype/add_parses/launch.py
+++ code/prototype/add_parses/launch.py
@@ -2,30 +2,40 @@ from py import pbs_util
 import argparse
 import subprocess
 
-parser = argparse.ArgumentParser(description='TODO')
-parser.add_argument('--article_list_file', type=str, required=True)
-parser.add_argument('--max_articles', type=int)
-args = parser.parse_args()
+def launch_job_for_slice(articles_slice):
+    job_command = ['prototype/add_parses/add_parses'] + articles_slice
+    job_id = pbs_util.launch_job(
+        # TODO: calculate walltime; parallelize
+        walltime="01:00:00",
+        node_spec="nodes=1:brno:ppn=4,mem=8gb",
+        job_name="add-parses",
+        job_command=job_command
+    )
+    print("Launched add-parses:", job_id)
 
-with open(args.article_list_file) as f:
-    article_names = list(map(lambda line: line.strip(), list(f)))
+def main():
+    parser = argparse.ArgumentParser(description='TODO')
+    parser.add_argument('--article_list_file', type=str, required=True)
+    parser.add_argument('--max_articles', type=int)
+    parser.add_argument('--articles_per_job', type=int)
+    # TODO: add max_jobs
+    args = parser.parse_args()
 
-if args.max_articles:
-    article_names = article_names[:args.max_articles]
+    with open(args.article_list_file) as f:
+        article_names = list(map(lambda line: line.strip(), list(f)))
 
-# fails to run Java on remote machines :(
+    if args.max_articles:
+        article_names = article_names[:args.max_articles]
 
-job_command = [
-    'prototype/add_parses/add_parses',
-] + article_names
+    if not args.articles_per_job:
+        slices = [article_names]
+    else:
+        slices = []
+        for i in range(0, len(article_names), args.articles_per_job):
+            slices.append(article_names[i:i+args.articles_per_job])
 
-#subprocess.check_output(job_command)
+    for articles_slice in slices:
+        launch_job_for_slice(articles_slice)
 
-job_id = pbs_util.launch_job(
-    # TODO: calculate walltime; parallelize
-    walltime="01:00:00",
-    node_spec="nodes=1:brno:ppn=4,mem=8gb",
-    job_name="add-parses",
-    job_command=job_command
-)
-print("Launched add-parses:", job_id)
+if __name__ == '__main__':
+    main()
--- code/prototype/add_spotlight/launch.py
+++ code/prototype/add_spotlight/launch.py
@@ -1,35 +1,50 @@
 from py import pbs_util
 import argparse
 
-parser = argparse.ArgumentParser(description='TODO')
-parser.add_argument('--article_list_file', type=str, required=True)
-parser.add_argument('--max_articles', type=int)
-parser.add_argument('--spotlight_endpoint')
-args = parser.parse_args()
+def launch_job_for_slice(articles_slice, spotlight_endpoint):
+    job_command = ['prototype/add_spotlight/add_spotlight']
 
-with open(args.article_list_file) as f:
-    article_names = list(map(lambda line: line.strip(), list(f)))
+    if spotlight_endpoint:
+        job_command.extend(['--spotlight_endpoint', spotlight_endpoint])
 
-if args.max_articles:
-    article_names = article_names[:args.max_articles]
+    for name in articles_slice:
+        job_command.append('--articles')
+        job_command.append(name)
 
-job_command = [
-    'prototype/add_spotlight/add_spotlight',
-]
+    job_id = pbs_util.launch_job(
+        # TODO: calculate walltime; parallelize
+        walltime="01:00:00",
+        node_spec="nodes=1:brno:ppn=1,mem=1gb",
+        job_name="add-spotlight",
+        job_command=job_command
+    )
+    print("Launched add-spotlight:", job_id)
 
-if args.spotlight_endpoint:
-    job_command.extend(['--spotlight_endpoint', args.spotlight_endpoint])
+def main():
+    parser = argparse.ArgumentParser(description='TODO')
+    parser.add_argument('--article_list_file', type=str, required=True)
+    parser.add_argument('--max_articles', type=int)
+    parser.add_argument('--spotlight_endpoint')
+    # TODO: add max_jobs
+    parser.add_argument('--articles_per_job', type=int)
+    args = parser.parse_args()
 
-print(article_names)
-for name in article_names:
-    job_command.append('--articles')
-    job_command.append(name)
+    with open(args.article_list_file) as f:
+        article_names = list(map(lambda line: line.strip(), list(f)))
 
-job_id = pbs_util.launch_job(
-    # TODO: calculate walltime; parallelize
-    walltime="01:00:00",
-    node_spec="nodes=1:brno:ppn=1,mem=1gb",
-    job_name="add-spotlight",
-    job_command=job_command
-)
-print("Launched add-spotlight:", job_id)
+    if args.max_articles:
+        article_names = article_names[:args.max_articles]
+
+    if not args.articles_per_job:
+        slices = [article_names]
+    else:
+        slices = []
+        for i in range(0, len(article_names), args.articles_per_job):
+            slices.append(article_names[i:i+args.articles_per_job])
+
+    for articles_slice in slices:
+        launch_job_for_slice(articles_slice,
+                             spotlight_endpoint=args.spotlight_endpoint)
+
+if __name__ == '__main__':
+    main()
