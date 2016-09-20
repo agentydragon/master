b9c4248aeac4d5a8f90331e55d52aa6be34982f9
Michal Pokorný <pok@rny.cz>
2016-09-20 14:42:39 +0200

    more relations to train

--- code/prototype/train.py
+++ code/prototype/train.py
@@ -2,24 +2,30 @@ from prototype.lib import sample_repo
 import random
 import nltk
 
-# relations = [
-#     'P25', # mother
-#     'P22', # father
-#     'P7', # brother
-#     'P40', # child
-#     'P26', # spouse
-#     'P108', # employer
-#     'P710', # participant
-#     'P463', # member of
-#     'P27', # country of citizenship
-#     'P36', # capital
-#     'P31', # instance of
-#     'P1066', # student of
-#     'P551', # residence
-#     'P112', # founder
-#     'P115', # follows
-#     'P175' # performer
-# ]
+relations = [
+    'P25', # mother
+    'P22', # father
+    'P7', # brother
+    'P40', # child
+    'P26', # spouse
+    'P108', # employer
+    'P710', # participant
+    'P463', # member of
+    'P27', # country of citizenship
+    'P36', # capital
+    'P31', # instance of
+    'P1066', # student of
+    'P551', # residence
+    'P112', # founder
+    'P115', # follows
+    'P175', # performer
+    'P457', # member of political party
+    'P50', # author
+    'P19', # place of birth
+    'P361', # part of
+    'P86', # composer
+    'P57', # director
+]
 
 relations = sample_repo.all_relations()
 
@@ -38,6 +44,7 @@ def sample_to_features_label(sample):
 
 samples = []
 for relation in relations:
+    print('Loading relation', relation, '...')
     for sample in sample_repo.load_samples(relation):
         samples.append(sample_to_features_label(sample))
 
@@ -48,6 +55,7 @@ n_train = round(len(samples) * 0.5)
 train = samples[:n_train]
 test = samples[n_train:]
 
+print('Training...')
 classifier = nltk.NaiveBayesClassifier.train(train)
 print('Accuracy:', nltk.classify.accuracy(classifier, test))
 classifier.show_most_informative_features(10)
