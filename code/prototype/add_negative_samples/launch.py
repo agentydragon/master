268953fae2c24afab473c8517e63a092c6ce11c2
Michal Pokorný <pok@rny.cz>
2016-09-21 14:59:40 +0200

    require article list
    fa

--- code/prototype/add_negative_samples/launch.py
+++ code/prototype/add_negative_samples/launch.py
@@ -10,13 +10,15 @@ import datetime
 
 now = datetime.datetime.now()
 
-def launch_job_for_slice(i, relations_slice, wikidata_endpoint): #, parallelism):
+def launch_job_for_slice(i, article_list_file, relations_slice, wikidata_endpoint): #, parallelism):
     job_name = 'add-negative-samples'
 
-    log_base_dir = paths.LOG_PATH + "/job_name/" + now.strftime('%Y%m%d-%H%M%S')
+    log_base_dir = paths.LOG_PATH + "/" + job_name + "/" + now.strftime('%Y%m%d-%H%M%S')
     file_util.ensure_dir(log_base_dir)
 
-    job_command = ['prototype/add_negative_samples/add_negative_samples']
+    job_command = ['prototype/add_negative_samples/add_negative_samples'
+                   '--article_list_file', article_list_file,
+                   ]
     if wikidata_endpoint:
         job_command.extend(['--wikidata_endpoint', wikidata_endpoint])
     for relation in relations_slice:
@@ -41,6 +43,7 @@ def main():
     parser = argparse.ArgumentParser(description='TODO')
     parser.add_argument('--relations_per_job', type=int)
     parser.add_argument('--wikidata_endpoint')
+    parser.add_argument('--article_list_file', required=True)
     args = parser.parse_args()
 
     relations = sample_repo.all_relations()
@@ -53,7 +56,7 @@ def main():
             slices.append(relations[i:i+args.relations_per_job])
 
     for i, relation_slice in enumerate(slices):
-        launch_job_for_slice(i, relation_slice, args.wikidata_endpoint)
+        launch_job_for_slice(i, args.article_list_file, relation_slice, args.wikidata_endpoint)
 
 if __name__ == '__main__':
     main()
