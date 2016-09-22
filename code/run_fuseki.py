29b28f96aeff8b88afd7cd5b6a75febd9742b31e
Michal Pokorný <pok@rny.cz>
2016-09-22 16:48:48 +0200

    paths lib
    fix
    
    Squash 6 noisy commits

--- code/run_fuseki.py
+++ code/run_fuseki.py
@@ -2,9 +2,10 @@ import paths
 
 import os
 
+os.chdir('../jena_fuseki/apache-jena-fuseki-2.4.0')
 os.execv("/bin/bash", [
-    "apache-jena-fuseki-2.4.0/fuseki-server",
-    "--loc",
-    paths.WORK_DIR + "/fuseki-datasets/wikidata",
-    "/wikidata",
+    "bash",
+    "-c",
+    ("./fuseki-server --loc " + paths.WORK_DIR +
+     "/fuseki-datasets/wikidata /wikidata"),
 ])
