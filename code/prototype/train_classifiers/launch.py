f15bfdc741e2a76eda0e11181e261a4c1ccc9e4f
Michal Pokorný <pok@rny.cz>
2016-09-22 17:00:12 +0200

    launcher

---
+++ code/prototype/train_classifiers/launch.py
@@ -0,0 +1,33 @@
+from prototype.lib import sample_repo
+from prototype.lib import mapper
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='TODO')
+    parser.add_argument('--relations_per_job', type=int)
+    parser.add_argument('--local_parallelism', type=int, default=4)
+    args = parser.parse_args()
+
+    def make_commandline(relations_slice):
+        job_command = [
+            'prototype/train_classifiers/train_classifiers',
+            '--parallelism', str(args.local_parallelism)
+        ]
+        for relation in relations_slice:
+            job_command.extend(['--relation', relation])
+        return job_command
+
+    def slice_to_walltime(relations_slice):
+        # TODO: more precise
+        return "01:00:00"
+
+    mapper.launch_in_slices('train-classifiers',
+                            sample_repo.all_relations(),
+                            args.relations_per_job,
+                            make_commandline,
+                            slice_to_walltime,
+                            cores=max(2, args.local_parallelism),
+                            ram='2gb')
+
+if __name__ == '__main__':
+    main()
