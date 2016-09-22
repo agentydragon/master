df05737d888f23b64f55770b8d57284171a28dcf
Michal Pokorný <pok@rny.cz>
2016-09-22 16:54:50 +0200

    fuseki stuff

--- code/run_fuseki.py
+++ code/run_fuseki.py
@@ -2,10 +2,11 @@ import paths
 
 import os
 
+dataset_path = paths.WORK_DIR + '/fuseki-datasets/wikidata'
+
 os.chdir('../jena_fuseki/apache-jena-fuseki-2.4.0')
 os.execv("/bin/bash", [
     "bash",
     "-c",
-    ("./fuseki-server --loc " + paths.WORK_DIR +
-     "/fuseki-datasets/wikidata /wikidata"),
+    ("./fuseki-server --loc " + dataset_path + " /wikidata"),
 ])
