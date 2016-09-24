a38d71e14668539e6cc93492e1c3f50554a973c0
Michal Pokorný <pok@rny.cz>
2016-09-24 11:11:30 +0200

    fuseki run writes into zookeeper

--- code/run_fuseki.py
+++ code/run_fuseki.py
@@ -1,7 +1,20 @@
 import paths
 from thirdparty import fuseki
 
-fuseki.serve(
+from kazoo import client as kazoo_client
+
+print('Connecting to ZooKeeper...')
+kz = kazoo_client.KazooClient()
+kz.start()
+
+kz.ensure_path('/user/prvak/thesis')
+kz.create(
+    '/user/prvak/thesis/wikidata-service',
+    b'hador:3030',
+    makepath = True
+)
+
+fuseki.serve_forever(
     dataset_path = paths.WORK_DIR + '/fuseki-datasets/wikidata',
     namespace = '/wikidata'
 )
--- code/thirdparty/fuseki/fuseki.py
+++ code/thirdparty/fuseki/fuseki.py
@@ -1,6 +1,7 @@
 import os
+from prototype.lib import pbs_util
 
-def serve(dataset_path, namespace):
+def serve_forever(dataset_path, namespace):
     os.chdir('../jena_fuseki/apache-jena-fuseki-2.4.0')
     os.execv("/bin/bash", [
         "bash",
