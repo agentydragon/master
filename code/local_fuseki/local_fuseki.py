ac9112977ac1204898299bb1cb17cb7aaf4aa269
Michal Pokorný <pok@rny.cz>
2016-09-24 22:05:07 +0200

    add local_fuseki launcher

---
+++ code/local_fuseki/local_fuseki.py
@@ -0,0 +1,23 @@
+import paths
+from thirdparty.fuseki import fuseki
+import subprocess
+import os
+
+scratch_dir = os.environ['SCRATCHDIR']
+
+print("Copying Wikidata to local disk...")
+rv = subprocess.call([
+    "cp",
+    "-rv",
+    paths.WORK_DIR + '/fuseki-datasets/wikidata',
+    scratch_dir + '/wikidata'
+])
+print("Copied.")
+assert rv == 0
+
+print("Starting Fuseki...")
+fuseki.serve_forever(
+    dataset_path = scratch_dir + '/fuseki-datasets/wikidata',
+    namespace = '/wikidata',
+    port = 3030
+)
---
+++ code/local_fuseki/local_fuseki_launch.py
@@ -0,0 +1,9 @@
+from prototype.lib import pbs_util
+
+job = pbs_util.launch(
+    walltime = "02:00:00",
+    node_spec = "nodes=1:brno:ppn=4,mem=4gb,scratch=200gb",
+    job_name = "local_fuseki",
+    script = "../cpulimit/cpulimit -l 400 local_fuseki/local_fuseki",
+)
+print(job.job_id)
