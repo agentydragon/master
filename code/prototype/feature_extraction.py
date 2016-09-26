5dd868a72161c5fdf42c05a47d3594c3b631406d
Michal Pokorný <pok@rny.cz>
2016-09-26 11:29:50 +0200

    refactoring

--- code/prototype/feature_extraction.py
+++ code/prototype/feature_extraction.py
@@ -1,4 +1,4 @@
-def sample_to_features(sample):
+def sentence_bag_of_words_features(sample):
     features = set()
     for i, token in enumerate(sample.sentence.tokens):
         # debias
@@ -10,47 +10,45 @@ def sample_to_features(sample):
         features.add('lemma_' + token.lemma.lower())
         word = sample.sentence.text[token.start_offset:token.end_offset].lower()
         features.add('word_' + word)
+    return features
 
-    # window before subject
+def window_features(prefix, indices, sample):
+    features = set()
+    # window before
     for i in range(-2, 0):
-        idx = min(sample.subject_token_indices) - i
+        idx = min(indices) - i
         if idx not in range(len(sample.sentence.tokens)):
             continue
-        features.add('subject_window_%d_lemma_%s' % (i, token.lemma))
+        features.add(prefix + '_window_%d_lemma_%s' % (i, token.lemma))
         word = sample.sentence.text[token.start_offset:token.end_offset].lower()
-        features.add('subject_window_%d_word_%s' % (i, word))
-
-    # window before object
-    for i in range(-2, 0):
-        idx = min(sample.object_token_indices) - i
-        if idx not in range(len(sample.sentence.tokens)):
-            continue
-        features.add('object_window_%d_lemma_%s' % (i, token.lemma))
-        word = sample.sentence.text[token.start_offset:token.end_offset].lower()
-        features.add('object_window_%d_word_%s' % (i, word))
-
-    # window after subject
+        features.add(prefix + '_window_%d_word_%s' % (i, word))
+    # window after
     for i in range(1, 3):
-        idx = max(sample.subject_token_indices) + i
+        idx = max(indices) + i
         if idx not in range(len(sample.sentence.tokens)):
             continue
-        features.add('subject_window_%d_lemma_%s' % (i, token.lemma))
+        features.add(prefix + '_window_%d_lemma_%s' % (i, token.lemma))
         word = sample.sentence.text[token.start_offset:token.end_offset].lower()
-        features.add('subject_window_%d_word_%s' % (i, word))
+        features.add(prefix + '_window_%d_word_%s' % (i, word))
+    return features
 
-    # window after object
-    for i in range(1, 3):
-        idx = max(sample.object_token_indices) + i
-        if idx not in range(len(sample.sentence.tokens)):
-            continue
-        features.add('object_window_%d_lemma_%s' % (i, token.lemma))
-        word = sample.sentence.text[token.start_offset:token.end_offset].lower()
-        features.add('object_window_%d_word_%s' % (i, word))
+def sample_to_features(sample):
+    features = set()
+    # bag-of-words features
+    features = features.union(sentence_bag_of_words_features(sample))
+
+    # subject window
+    subject_window = window_features('subject', sample.subject_token_indices, sample)
+    features = features.union(subject_window)
+    # object window
+    object_window = window_features('object', sample.object_token_indices, sample)
+    features = features.union(object_window)
 
     if min(sample.subject_token_indices) < min(sample.object_token_indices):
         features.add('subject_first')
     else:
         features.add('object_first')
+
     return features
 
 def sample_to_features_label(sample):
