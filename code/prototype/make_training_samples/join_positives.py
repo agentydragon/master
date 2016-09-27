4653d81b7c81d5b70d26424c402ca8412f5bc287
Michal Pokorný <pok@rny.cz>
2016-09-27 14:11:27 +0200

    allow short-circuit
    fasfd

--- code/prototype/make_training_samples/join_positives.py
+++ code/prototype/make_training_samples/join_positives.py
@@ -1,13 +1,20 @@
 from prototype.lib import sample_repo
+from prototype.lib import relations as relations_lib
 from prototype.lib import flags
 import progressbar
 
 def main():
+    flags.add_argument('--relation', action='append')
     flags.make_parser(description='TODO')
     args = flags.parse_args()
 
+    if args.relation:
+        relations = args.relation
+    else:
+        relations = relations_lib.RELATIONS
+
     bar = progressbar.ProgressBar(redirect_stdout=True)
-    for relation in bar(sample_repo.all_relations()):
+    for relation in bar(relations):
         samples = sample_repo.load_positive_samples_by_articles(relation)
         sample_repo.write_positive_samples(relation, samples)
         print(relation, 'positives joined', len(samples))
