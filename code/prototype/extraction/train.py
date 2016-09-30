1a441e01256646b83bfa2f524fc84d4fbc2dadbc
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-09-30 09:57:02 +0200

    Comment out newly broken files dependent on joins

--- code/prototype/extraction/train.py
+++ code/prototype/extraction/train.py
@@ -1,97 +1,97 @@
-from prototype.lib import sample_repo
-from prototype.extraction import feature_extraction
-import random
-import nltk
-import nltk.classify.decisiontree
-import nltk.classify.maxent
-
-relations = [
-    'P25', # mother
-    'P22', # father
-    'P7', # brother
-    'P40', # child
-    'P26', # spouse
-]
-
-#     'P108', # employer
-#     'P710', # participant
-#     'P463', # member of
-#     'P27', # country of citizenship
-#     'P36', # capital
-#     'P31', # instance of
-#     'P1066', # student of
-#     'P551', # residence
-#     'P112', # founder
-#     'P115', # follows
-#     'P175', # performer
-#     'P457', # member of political party
-#     'P50', # author
-#     'P19', # place of birth
-#     'P361', # part of
-#     'P86', # composer
-#     'P57', # director
-#     'P463', # member of
-#     'P69', # educated at
-#     'P159', # headquarters location
-#     'P140', # religion
-#     'P800', # notable work
+# from prototype.lib import sample_repo
+# from prototype.extraction import feature_extraction
+# import random
+# import nltk
+# import nltk.classify.decisiontree
+# import nltk.classify.maxent
+# 
+# relations = [
+#     'P25', # mother
+#     'P22', # father
+#     'P7', # brother
+#     'P40', # child
+#     'P26', # spouse
 # ]
-
-# relations = sample_repo.all_relations()
-
-#samples = sample_repo.load_samples(mother)
-#for sample in samples:
-#    lemmas = []
-#    for i, token in enumerate(sample.sentence.tokens):
-#        lemmas.append(token.lemma)
-#    print(" ".join(lemmas))
-
-def cull_uninformative_features(samples):
-    feature_counts = {}
-    for sample in samples:
-        for feature in list(sample[0].keys()):
-            if feature not in feature_counts:
-                feature_counts[feature] = 0
-            feature_counts[feature] += 1
-
-    min_feature_occurrences = 10
-
-    for sample in samples:
-        for feature in list(sample[0].keys()):
-            if feature_counts[feature] < min_feature_occurrences:
-                del sample[0][feature]
-
-samples = []
-for relation in relations:
-    print('Loading relation', relation, '...')
-    relation_samples = sample_repo.load_samples(relation)
-    relation_samples = list(filter(lambda sample: sample.positive, relation_samples))
-    for sample in relation_samples:
-        samples.append(feature_extraction.sample_to_features_label(sample))
-    print(len(relation_samples))
-
-random.shuffle(samples)
-cull_uninformative_features(samples)
-
-print("Samples:", len(samples))
-
-n_train = round(len(samples) * 0.5)
-train = samples[:n_train]
-test = samples[n_train:]
-print("N training samples:", len(train), "test:", len(test))
-
-print('Training naive Bayes...')
-classifier = nltk.NaiveBayesClassifier.train(train)
-classifier.show_most_informative_features(10)
-print('Accuracy:', nltk.classify.accuracy(classifier, test))
-
-print('Training decision tree...')
-classifier = nltk.classify.decisiontree.DecisionTreeClassifier.train(
-    train, depth_cutoff=4, binary=True)
+# 
+# #     'P108', # employer
+# #     'P710', # participant
+# #     'P463', # member of
+# #     'P27', # country of citizenship
+# #     'P36', # capital
+# #     'P31', # instance of
+# #     'P1066', # student of
+# #     'P551', # residence
+# #     'P112', # founder
+# #     'P115', # follows
+# #     'P175', # performer
+# #     'P457', # member of political party
+# #     'P50', # author
+# #     'P19', # place of birth
+# #     'P361', # part of
+# #     'P86', # composer
+# #     'P57', # director
+# #     'P463', # member of
+# #     'P69', # educated at
+# #     'P159', # headquarters location
+# #     'P140', # religion
+# #     'P800', # notable work
+# # ]
+# 
+# # relations = sample_repo.all_relations()
+# 
+# #samples = sample_repo.load_samples(mother)
+# #for sample in samples:
+# #    lemmas = []
+# #    for i, token in enumerate(sample.sentence.tokens):
+# #        lemmas.append(token.lemma)
+# #    print(" ".join(lemmas))
+# 
+# def cull_uninformative_features(samples):
+#     feature_counts = {}
+#     for sample in samples:
+#         for feature in list(sample[0].keys()):
+#             if feature not in feature_counts:
+#                 feature_counts[feature] = 0
+#             feature_counts[feature] += 1
+# 
+#     min_feature_occurrences = 10
+# 
+#     for sample in samples:
+#         for feature in list(sample[0].keys()):
+#             if feature_counts[feature] < min_feature_occurrences:
+#                 del sample[0][feature]
+# 
+# samples = []
+# for relation in relations:
+#     print('Loading relation', relation, '...')
+#     relation_samples = sample_repo.load_samples(relation)
+#     relation_samples = list(filter(lambda sample: sample.positive, relation_samples))
+#     for sample in relation_samples:
+#         samples.append(feature_extraction.sample_to_features_label(sample))
+#     print(len(relation_samples))
+# 
+# random.shuffle(samples)
+# cull_uninformative_features(samples)
+# 
+# print("Samples:", len(samples))
+# 
+# n_train = round(len(samples) * 0.5)
+# train = samples[:n_train]
+# test = samples[n_train:]
+# print("N training samples:", len(train), "test:", len(test))
+# 
+# print('Training naive Bayes...')
+# classifier = nltk.NaiveBayesClassifier.train(train)
 # classifier.show_most_informative_features(10)
-print('Accuracy:', nltk.classify.accuracy(classifier, test))
-
-print('Training maxent...')
-classifier = nltk.classify.maxent.MaxentClassifier.train(train)
-# classifier.show_most_informative_features(10)
-print('Accuracy:', nltk.classify.accuracy(classifier, test))
+# print('Accuracy:', nltk.classify.accuracy(classifier, test))
+# 
+# print('Training decision tree...')
+# classifier = nltk.classify.decisiontree.DecisionTreeClassifier.train(
+#     train, depth_cutoff=4, binary=True)
+# # classifier.show_most_informative_features(10)
+# print('Accuracy:', nltk.classify.accuracy(classifier, test))
+# 
+# print('Training maxent...')
+# classifier = nltk.classify.maxent.MaxentClassifier.train(train)
+# # classifier.show_most_informative_features(10)
+# print('Accuracy:', nltk.classify.accuracy(classifier, test))
--- code/prototype/visualize_relation.py
+++ code/prototype/visualize_relation.py
@@ -3,78 +3,78 @@ from prototype.lib import flags
 from prototype.lib import wikidata
 import json
 
-def show_all_relations(client):
-    for relation in sample_repo.all_relations():
-        samples = sample_repo.load_samples(relation)
-        positive = list(filter(lambda sample: sample.positive, samples))
-        negative = list(filter(lambda sample: not sample.positive, samples))
-        print(relation,
-              client.get_name(relation), ":",
-              len(samples), "samples",
-              len(positive), "positive",
-              len(negative), "negative")
-
-def show_relation(client, relation):
-    samples = sample_repo.load_samples(relation)
-
-    html = ""
-    html += """
-    <style>
-    li.positive { color: darkgreen; }
-    li.negative { color: darkred; }
-    .token.subject { text-decoration: underline; color: #999; }
-    .token.object { text-style: italic; color: #999; }
-    </style>
-    """
-    html += "<h1>" + relation + ": " + client.get_name(relation) + "</h1>"
-
-    for sample in samples:
-        html += "<li class='%s'>" % ('positive' if sample.positive else
-                                     'negative')
-        html += sample.subject + " " + sample.object + " "
-        # html += sample.sentence.text
-        arry = sample.to_json()
-        del arry['sentence']['tokens']
-        del arry['sentence']['text']
-        del arry['subject_token_indices']
-        del arry['object_token_indices']
-        del arry['subject']
-        del arry['object']
-        del arry['relation']
-        html += str(arry)
-        html += "<div class='tokens'>"
-        for i, token in enumerate(sample.sentence.tokens):
-            classes = []
-            if i in sample.subject_token_indices:
-                classes.append('subject')
-            if i in sample.object_token_indices:
-                classes.append('object')
-
-            start = token.start_offset
-            end = token.end_offset
-            text = sample.sentence.text[start:end]
-            ary = token.to_json()
-            del ary['start_offset']
-            del ary['end_offset']
-            html += "<span class='token " + (" ".join(classes)) + "' data-json='" + json.dumps(ary) + "'>" + text + "</span>"
-            html += " "
-        html += "</div>"
-
-
-    print(html)
-
-def main():
-    flags.add_argument('--relation')
-    flags.make_parser(description='TODO')
-    args = flags.parse_args()
-
-    client = wikidata.WikidataClient()
-
-    if args.relation:
-        relation = args.relation
-        show_relation(client, relation)
-    else:
-        show_all_relations(client)
+#def show_all_relations(client):
+#    for relation in sample_repo.all_relations():
+#        samples = sample_repo.load_samples(relation)
+#        positive = list(filter(lambda sample: sample.positive, samples))
+#        negative = list(filter(lambda sample: not sample.positive, samples))
+#        print(relation,
+#              client.get_name(relation), ":",
+#              len(samples), "samples",
+#              len(positive), "positive",
+#              len(negative), "negative")
+#
+#def show_relation(client, relation):
+#    samples = sample_repo.load_samples(relation)
+#
+#    html = ""
+#    html += """
+#    <style>
+#    li.positive { color: darkgreen; }
+#    li.negative { color: darkred; }
+#    .token.subject { text-decoration: underline; color: #999; }
+#    .token.object { text-style: italic; color: #999; }
+#    </style>
+#    """
+#    html += "<h1>" + relation + ": " + client.get_name(relation) + "</h1>"
+#
+#    for sample in samples:
+#        html += "<li class='%s'>" % ('positive' if sample.positive else
+#                                     'negative')
+#        html += sample.subject + " " + sample.object + " "
+#        # html += sample.sentence.text
+#        arry = sample.to_json()
+#        del arry['sentence']['tokens']
+#        del arry['sentence']['text']
+#        del arry['subject_token_indices']
+#        del arry['object_token_indices']
+#        del arry['subject']
+#        del arry['object']
+#        del arry['relation']
+#        html += str(arry)
+#        html += "<div class='tokens'>"
+#        for i, token in enumerate(sample.sentence.tokens):
+#            classes = []
+#            if i in sample.subject_token_indices:
+#                classes.append('subject')
+#            if i in sample.object_token_indices:
+#                classes.append('object')
+#
+#            start = token.start_offset
+#            end = token.end_offset
+#            text = sample.sentence.text[start:end]
+#            ary = token.to_json()
+#            del ary['start_offset']
+#            del ary['end_offset']
+#            html += "<span class='token " + (" ".join(classes)) + "' data-json='" + json.dumps(ary) + "'>" + text + "</span>"
+#            html += " "
+#        html += "</div>"
+#
+#
+#    print(html)
+#
+#def main():
+#    flags.add_argument('--relation')
+#    flags.make_parser(description='TODO')
+#    args = flags.parse_args()
+#
+#    client = wikidata.WikidataClient()
+#
+#    if args.relation:
+#        relation = args.relation
+#        show_relation(client, relation)
+#    else:
+#        show_all_relations(client)
 
 if __name__ == '__main__':
     main()
