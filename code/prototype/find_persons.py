45bb5090d6e10d5329f4aafc213f9689020884dc
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-05 19:30:06 +0200

    Fix find_persons

--- code/prototype/find_persons.py
+++ code/prototype/find_persons.py
@@ -6,42 +6,55 @@ Usage:
 from prototype.lib import wikidata
 from prototype.lib import wikidata_util
 from prototype.lib import flags
+import paths
 import sys
 import progressbar
 
-flags.add_argument('--num_persons', type=int, default=10000)
-flags.make_parser(description='TODO')
-args = flags.parse_args()
+def main():
+    flags.add_argument('--num_persons', type=int, default=10000)
+    flags.add_argument('--write', type=bool, default=False)
+    flags.make_parser(description='TODO')
+    args = flags.parse_args()
 
-wikidata_client = wikidata.WikidataClient()
-print("Looking for %d persons..." % args.num_persons)
+    wikidata_client = wikidata.WikidataClient()
+    print("Looking for %d persons..." % args.num_persons)
 
-results = wikidata_client.wikidata_client.get_result_values("""
-    SELECT *
-    WHERE {
-        # instance-of human
-        ?person wdp:P31 wd:Q5 .
+    results = wikidata_client.wikidata_client.get_result_values("""
+        SELECT *
+        WHERE {
+            # instance-of human
+            ?person wdp:P31 wd:Q5 .
 
-        ?person rdfs:label ?label .
-        FILTER (lang(?label) = "en")
-    }
-    LIMIT %d
-""" % args.num_persons)
+            ?person rdfs:label ?label .
+            FILTER (lang(?label) = "en")
+        }
+        LIMIT %d
+    """ % args.num_persons)
 
-#         ?person owl:sameAs ?dbpedia_person .
-#         ?dbpedia_person foaf:isPrimaryTopicOf ?wikipedia_page .
+    #         ?person owl:sameAs ?dbpedia_person .
+    #         ?dbpedia_person foaf:isPrimaryTopicOf ?wikipedia_page .
 
-bar = progressbar.ProgressBar()
-persons = set()
-for row in bar(results):
-    id = wikidata_util.wikidata_entity_url_to_entity_id(row['person'])
-    name = row['label']
-#    wikipedia_page = row['wikipedia_page']
-    wikipedia_page = '???'
-    persons.add((name, id, wikipedia_page)) # add: because of multiplicities
+    bar = progressbar.ProgressBar()
+    persons = set()
+    for row in bar(results):
+        id = wikidata_util.wikidata_entity_url_to_entity_id(row['person'])
+        name = row['label']
+    #    wikipedia_page = row['wikipedia_page']
+        wikipedia_page = '???'
+        persons.add((name, id, wikipedia_page)) # add: because of multiplicities
 
-    print(name, id, wikipedia_page)
+        print(name, id, wikipedia_page)
 
-for name, id, wikipedia_page in sorted(persons):
-    print('%s\t%s' % (id, name))
-    sys.stdout.flush()
+    f = sys.stdout
+    if flags.parse_args().write:
+        f = open(paths.ARTICLE_LIST_PATH, 'w')
+
+    for name, id, wikipedia_page in sorted(persons):
+        f.write('%s\t%s\t%s\n' % (id, name, wikipedia_page))
+        f.flush()
+
+    if flags.parse_args().write:
+        f.close()
+
+if __name__ == '__main__':
+    main()
