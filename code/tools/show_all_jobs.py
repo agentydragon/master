1bbe16342d2e9498ebc01cd36b8c204943646516
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-10 17:10:42 +0200

    Nicer job pretty-printing

---
+++ code/tools/show_all_jobs.py
@@ -0,0 +1,16 @@
+from prototype.lib import pbs_util
+
+data = []
+data.append(['Status', 'ID', 'Name', 'Machine', 'Runtime', 'Target runtime'])
+for job in pbs_util.get_all_jobs():
+    data.append([
+        job['status'],
+        job['jobid'],
+        job['jobname'],
+        job['job_machine'],
+        job['runtime'],
+        job['target_walltime']
+    ])
+
+import terminaltables
+print(terminaltables.AsciiTable(data).table)
