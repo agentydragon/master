f19eebe0c23af6422a7c67eabbbc7957927e7d84
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-11 19:45:45 +0200

    Add note about runtime

--- code/prototype/kb/launch_fill_fuseki_dataset.py
+++ code/prototype/kb/launch_fill_fuseki_dataset.py
@@ -2,6 +2,7 @@ from prototype.lib import pbs_util
 
 # 2016-10-07: creating merged dataset took 25 000 seconds on Hador's HDD
 # 2016-10-07: 48gb of RAM is not enough, just after finishing load phase. (sort phase limit applied)
+# 2016-10-11: took 7:43:56 to finish, while running on screen in Hador
 
 # walltime: 168h
 # mem: 24gb
