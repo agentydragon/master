5c31c7ca63b12f1a504425d4cd4a7ea22136ef1f
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-12 15:31:00 +0200

    Remove dead comments from WikiSplit

--- code/prototype/split_wiki/WikiSplit.java
+++ code/prototype/split_wiki/WikiSplit.java
@@ -1,19 +1,17 @@
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
-import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.hbase.HBaseConfiguration;
 import java.lang.System;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.BasicConfigurator;
 
 public class WikiSplit extends Configured implements Tool {
 	// Custom InputFormat that processes entire file in blocks.
@@ -34,20 +32,10 @@ public class WikiSplit extends Configured implements Tool {
 		job.setInputFormatClass(MyFIF.class);
 		MyFIF.addInputPath(job, new Path(args[0]));
 
-		// Set mapper and input/output classes.
 		job.setMapperClass(ArticleSplitterMapper.class);
-		//job.setMapOutputValueClass(Put.class);
 		job.setNumReduceTasks(0);
 
-		//job.setOutputKeyClass(Text.class);
-		//job.setOutputValueClass(Text.class);
-		//job.setOutputValueClass(Put.class);
-
-		//job.setNumReduceTasks(0);
-
 		// Set output.
-		//job.setOutputFormatClass(SequenceFileOutputFormat.class);
-		//TextOutputFormat.setOutputPath(job, new Path(args[1]));
 		job.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "prvak:wiki_articles");
 		job.setOutputFormatClass(TableOutputFormat.class);
 
@@ -65,6 +53,7 @@ public class WikiSplit extends Configured implements Tool {
 		conf.set("hbase.security.authentication", "kerberos");
 		conf.set("hbase.kerberos.regionserver.principal", "hbase/_HOST@ICS.MUNI.CZ");
 		conf.set("hbase.sasl.clientconfig", "Client");
+
 		int res = ToolRunner.run(conf, new WikiSplit(), args);
 		System.exit(res);
 	}
