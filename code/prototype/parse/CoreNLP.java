0e252d4c0a38554de3c631af487dc843d22feeb9
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-12 15:30:08 +0200

    Working CoreNLP annotation on Hadoop

--- code/prototype/parse/CoreNLP.java
+++ code/prototype/parse/CoreNLP.java
@@ -1,62 +1,72 @@
 import java.lang.System;
+import java.util.Arrays;
+import java.io.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.JobContext;
-import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.BasicConfigurator;
 
-// Input:
-//   Key:   article name (Text)
-//   Value: article plaintext (Text)
-//
-// Output:
-//   Key:   article name (Text)
-//   Value: article parse from CoreNLP as XML (Text)
-//
-// Arguments: (input) (output) -Dprefix_length=200
+// Arguments: -Dprefix_length=200
 
 public class CoreNLP extends Configured implements Tool {
 	public int run(String[] args) throws Exception {
-		// Configuration processed by ToolRunner
 		Configuration conf = getConf();
-		for (int i = 0; i < args.length; i++) {
-			System.out.println("args[" + i + "]=" + args[i]);
-		}
+
 		if (conf.get("prefix_length") == null) {
 			System.out.println("No prefix_length given");
 			return 1;
 		}
 
-		// Create a JobConf using the processed conf
-		Job job = Job.getInstance(conf, "corenlp-annotate");
+		Job job = Job.getInstance(conf, CoreNLP.class.getName());
 		job.setJarByClass(CoreNLP.class);
 
-		job.setInputFormatClass(SequenceFileInputFormat.class);
-		SequenceFileInputFormat.addInputPath(job, new Path(args[0]));
+		Scan scanner = new Scan();
+		scanner.setStartRow("Andrew Wiles".getBytes());
+		scanner.setStopRow("Autism".getBytes());
+		scanner.addColumn("wiki".getBytes(), "plaintext".getBytes());
+		// TODO: get only latest version; or overwrite.
 
-		job.setOutputFormatClass(SequenceFileOutputFormat.class);
-		SequenceFileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setNumReduceTasks(0);
 
-		job.setMapperClass(CoreNLPAnnotateMapper.class);
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		TableMapReduceUtil.initCredentials(job);
 
-		// Submit the job, then poll for progress until the job is complete
+		// Set output.
+		job.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "prvak:wiki_articles");
+		job.setOutputFormatClass(TableOutputFormat.class);
+
+		TableMapReduceUtil.initTableMapperJob(
+				"prvak:wiki_articles".getBytes(),
+				scanner,
+				CoreNLPAnnotateMapper.class,
+				ImmutableBytesWritable.class,
+				Put.class,
+				job);
+
+		Configuration.dumpConfiguration(job.getConfiguration(), new OutputStreamWriter(System.out));
+
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
 
 	public static void main(String[] args) throws Exception {
-		// Let ToolRunner handle generic command-line options
+		BasicConfigurator.configure();
+
+		Configuration conf = HBaseConfiguration.create();
+		conf.set("hbase.zookeeper.quorum", "hador-c1.ics.muni.cz:2181,hador-c2.ics.muni.cz:2181,hador.ics.muni.cz:2181");
+		conf.setBoolean("hbase.security.auth.enable", true);
+		conf.set("hbase.security.authentication", "kerberos");
+		conf.set("hbase.kerberos.regionserver.principal", "hbase/_HOST@ICS.MUNI.CZ");
+		conf.set("hbase.sasl.clientconfig", "Client");
+
 		int res = ToolRunner.run(null, new CoreNLP(), args);
 		System.exit(res);
 	}
--- code/prototype/parse/CoreNLPAnnotateMapper.java
+++ code/prototype/parse/CoreNLPAnnotateMapper.java
@@ -1,11 +1,19 @@
 import java.io.IOException;
 
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
 
-public class CoreNLPAnnotateMapper extends Mapper<Text, Text, Text, Text>{
+//public class CoreNLPAnnotateMapper extends Mapper<Text, Text, Text, Text>{
+
+// output key: K
+// output value: Put
+public class CoreNLPAnnotateMapper/*<K>*/ extends TableMapper</*K*/ImmutableBytesWritable, Put>{
 	private int prefixLength;
 	private CoreNLPInterface corenlpInterface = new CoreNLPInterface();
 
@@ -17,19 +25,24 @@ public class CoreNLPAnnotateMapper extends Mapper<Text, Text, Text, Text>{
 	}
 
 	@Override
-	public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-		String articleTitle = key.toString();
-		String articleText = value.toString();
+	public void map(ImmutableBytesWritable rowkey, Result result, Context context) throws IOException, InterruptedException {
+		//String articleText = value.toString();
+		String articleText = new String(result.getValue("wiki".getBytes(), "plaintext".getBytes()));
 
 		// Reduce the length of the text.
 		// XXX: HAX
 		int length = articleText.length();
-		if (length > prefixLength) {
+		if (prefixLength >= 0 && length > prefixLength) {
 			length = prefixLength;
 		}
 		articleText = articleText.substring(0, length);
 		// articleText = "Jackdaws love my big sphinx on quartz.";
 
-		context.write(key, new Text(corenlpInterface.getXML(articleText)));
+		Put put = new Put(rowkey.get());
+		String xml = corenlpInterface.getXML(articleText);
+		put.add("wiki".getBytes(), "corenlp_xml".getBytes(), xml.getBytes());
+		// put.add("wiki".getBytes(), "corenlp_xml".getBytes(), articleText.getBytes());
+		// context.write(key, new Text(xml));
+		context.write(null, put);
 	}
 }
