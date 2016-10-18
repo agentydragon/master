e8f23a9b2c04ddc7b8608150d961aa654a36d9c6
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-18 11:49:21 +0200

    Give more cores to entity recognition

--- code/prototype/entity_recognition/launch.py
+++ code/prototype/entity_recognition/launch.py
@@ -24,17 +24,16 @@ def main():
         return job_command
 
     # (2016-10-10 prvak) 4 cores crash on startup sometimes (not sure how, cpulimit should prevent that)
+    # (2016-10-10 prvak) 8 cores also crash on startup, most of the time. using 12.
+    # (2016-10-10 prvak) 12 cores are not enough
     mapper.launch_in_slices(
         'add-spotlight',
         art_set.article_names,
         args.articles_per_job,
         make_commandline,
         slice_to_walltime=(lambda s: "02:00:00"),
-        # cores=1,
-        cores=8,
-        # ram='1gb'
-        ram='16gb'
-        # TODO: scratch
+        cores=12,
+        ram='16gb',
     )
 
 if __name__ == '__main__':
