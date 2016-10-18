25118577395ba354ebf7e37b26cdb724fd60688a
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-18 11:46:41 +0200

    Remove TestHBase and add GetKeys

--- code/hadoop/GetKeys.java
+++ code/hadoop/GetKeys.java
@@ -2,13 +2,15 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
+import java.util.*;
 import org.apache.hadoop.hbase.util.*;
+import org.apache.hadoop.hbase.filter.*;
 
 // working as of 2016-10-11:
 // export HADOOP_CLASSPATH=`hbase classpath`
-// hadoop jar (...)/TestHBase_deploy.jar -Djava.security.auth.login.config=(...)/jaas.conf
+// hadoop jar (...)/GetKeys_deploy.jar -Djava.security.auth.login.config=(...)/jaas.conf
 
-public class TestHBase {
+public class GetKeys {
 	public static void main(String[] args) throws Exception {
 		BasicConfigurator.configure();
 
@@ -20,13 +22,30 @@ public class TestHBase {
 		conf.set("hbase.sasl.clientconfig", "Client");
 
 		HBaseAdmin admin = new HBaseAdmin(conf);
+		Scan s = new Scan();
+		FilterList fl = new FilterList();
+		fl.addFilter(new FirstKeyOnlyFilter());
+		fl.addFilter(new KeyOnlyFilter());
+		s.setFilter(fl);
+
+		List<String> keys = new ArrayList<>();
+
 		try {
 			HTable table = new HTable(conf, "prvak:wiki_articles");
-			Put put = new Put(Bytes.toBytes("Hello"));
-			put.add(Bytes.toBytes("wiki"), Bytes.toBytes("plaintext"), Bytes.toBytes("Hello hello ..."));
-			table.put(put);
+			ResultScanner rs = table.getScanner(s);
+			Result row = rs.next();
+			while (row != null) {
+				// System.out.println(new String(row.getRow()));
+				keys.add(new String(row.getRow()));
+				row = rs.next();
+			}
 		} finally {
 			admin.close();
+		}
+
+		int nShards = 676;
+		for (int i = 0; i < keys.size(); i += (keys.size() / nShards)) {
+			System.out.println(keys.get(i));
 		}
 	}
 }
