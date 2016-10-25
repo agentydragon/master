d25dcae8196d7fdc3d8bd04e94b468a7c5ec7dc2
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-25 16:46:43 +0200

    Simplify tool installation

---
+++ src/tools/README.md
@@ -0,0 +1,1 @@
+Run `./install.sh` to build these tools and install them into ~/bin.
--- src/tools/install.sh
+++ src/tools/install.sh
@@ -1,7 +1,15 @@
 # Special.
+rm -f ~/bin/run_nolock
 ln -s `pwd`/run_nolock.py ~/bin/run_nolock
 
+rm -f ~/bin/get_job_machine
 bazel run --script_path ~/bin/get_job_machine :get_job_machine
+
+rm -f ~/bin/kill_all_jobs
 bazel run --script_path ~/bin/kill_all_jobs :kill_all_jobs
+
+rm -f ~/bin/show_all_jobs
 bazel run --script_path ~/bin/show_all_jobs :show_all_jobs
-bazel run --script_path ~/bin/follow_logs :follow_logs
+
+rm -f ~/bin/follow_log
+bazel run --script_path ~/bin/follow_log :follow_log
