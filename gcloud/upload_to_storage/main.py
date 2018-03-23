03c055f4504497c8071102779dc05de6d7917b26
Michal Pokorný (Rai) <agentydragon@gmail.com>
2018-03-23 23:01:17 +0100

    Working download of a webpage to a gcloud file

--- gcloud/upload_to_storage/main.py
+++ gcloud/upload_to_storage/main.py
@@ -16,6 +16,7 @@ from __future__ import absolute_import
 
 from google.cloud import storage
 import six
+import urllib2
 
 
 def _get_storage_client():
@@ -26,13 +27,15 @@ def _get_storage_client():
 client = _get_storage_client()
 bucket = client.bucket("agentydragon-gspython")  # current_app.config['CLOUD_STORAGE_BUCKET'])
 import datetime
-filename = datetime.datetime.now().strftime("test-file-%Y%m%d%H%M%S")
+filename = datetime.datetime.now().strftime("test-file-%Y%m%d%H%M%S.html")
 blob = bucket.blob(filename)
 
-blob.upload_from_string(
-    "hello world",#file_stream,
-    content_type="text/plain")#content_type)
+http = urllib2.urlopen('http://agentydragon.github.io')
+blob.upload_from_file(
+    http,#"hello world",#file_stream,
+    content_type="text/html")#content_type)
 
 # upload_from_file: takes a file-like object
 
+print "Success! Downloaded the URL into Google Cloud Storage."
 print blob.public_url
