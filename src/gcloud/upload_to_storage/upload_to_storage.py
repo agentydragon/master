b3d3cf92f6c8e65037044f6559c5b6cdb759606d
Michal Pokorný (Rai) <agentydragon@gmail.com>
2018-03-24 23:34:50 +0100

    Format upload_to_storage.py by autopep8

--- src/gcloud/upload_to_storage/upload_to_storage.py
+++ src/gcloud/upload_to_storage/upload_to_storage.py
@@ -6,18 +6,22 @@ import urllib2
 url_to_download = 'https://dumps.wikimedia.org/enwiki/20180301/enwiki-20180301-pages-articles.xml.bz2'
 filename = 'wiki-dumps/enwiki/20180301/enwiki-20180301-pages-articles.xml.bz2'
 
+
 def _get_storage_client():
     return storage.Client(
         project='extended-atrium-198523')  # current_app.config['PROJECT_ID'])
 
+
 client = _get_storage_client()
-bucket = client.bucket("agentydragon-gspython")  # current_app.config['CLOUD_STORAGE_BUCKET'])
+# current_app.config['CLOUD_STORAGE_BUCKET'])
+bucket = client.bucket("agentydragon-gspython")
 http = urllib2.urlopen(url_to_download)
 blob = bucket.blob(filename, chunk_size=1024 * 1024)
 
 
 class UrlFile:
     """Wraps a urllib2 opened URL to enable uploading it by chunks."""
+
     def __init__(self, wrapped_file):
         self.wrapped_file = wrapped_file
         self.position = 0
