a6482c979596a7f3bd9ae4bc7376e4c80c6573a7
Michal Pokorný (Rai) <agentydragon@gmail.com>
2018-03-24 10:38:26 +0100

    Wrap urllib2's file in a wrapper to allow by-chunk upload

--- gcloud/upload_to_storage/main.py
+++ gcloud/upload_to_storage/main.py
@@ -18,28 +18,37 @@ from google.cloud import storage
 import six
 import urllib2
 
+url_to_download = 'https://dumps.wikimedia.org/enwiki/20180301/enwiki-20180301-pages-articles.xml.bz2'
+filename = 'wiki-dumps/enwiki/20180301/enwiki-20180301-pages-articles.xml.bz2'
 
 def _get_storage_client():
     return storage.Client(
         project='extended-atrium-198523')  # current_app.config['PROJECT_ID'])
 
-
 client = _get_storage_client()
 bucket = client.bucket("agentydragon-gspython")  # current_app.config['CLOUD_STORAGE_BUCKET'])
-#import datetime
-#filename = datetime.datetime.now().strftime("test-file-%Y%m%d%H%M%S.html")
-#blob = bucket.blob(filename)
-blob =
-bucket.blob('wiki-dumps/enwiki/20180301/enwiki-20180301-pages-articles.xml.bz2',
-            chunk_size=1024 * 1024)
+http = urllib2.urlopen(url_to_download)
+blob = bucket.blob(filename, chunk_size=1024 * 1024)
 
-http = urllib2.urlopen(#'http://agentydragon.github.io')
-    'https://dumps.wikimedia.org/enwiki/20180301/enwiki-20180301-pages-articles.xml.bz2')
-blob.upload_from_file(
-    http,#"hello world",#file_stream,
-    content_type="text/html")#content_type)
 
-# upload_from_file: takes a file-like object
+class UrlFile:
+    """Wraps a urllib2 opened URL to enable uploading it by chunks."""
+    def __init__(self, wrapped_file):
+        self.wrapped_file = wrapped_file
+        self.position = 0
+
+    def read(self, length):
+        print 'reading', length, 'bytes'
+        data = self.wrapped_file.read(length)
+        self.position += len(data)
+        print 'read', len(data), 'bytes, position=', self.position
+        return data
+
+    def tell(self):
+        return self.position
+
+
+blob.upload_from_file(UrlFile(http), content_type="text/html")
 
 print "Success! Downloaded the URL into Google Cloud Storage."
 print blob.public_url
