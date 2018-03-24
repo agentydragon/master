ea92f21a366c9dfa8e7ce2ffc18de1e9361512d0
Michal Pokorný (Rai) <agentydragon@gmail.com>
2018-03-24 12:44:31 +0100

    Way there...

---
+++ gcloud/upload_to_storage/create_single_file.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+from google.cloud import storage
+import six
+import urllib2
+import datetime
+
+filename = datetime.datetime.now().strftime('single-file-%Y%m%d-%H%M%S.txt')
+
+def _get_storage_client():
+    return storage.Client(
+        project='extended-atrium-198523')  # current_app.config['PROJECT_ID'])
+
+client = _get_storage_client()
+bucket = client.bucket("agentydragon-gspython")  # current_app.config['CLOUD_STORAGE_BUCKET'])
+blob = bucket.blob(filename)
+blob.upload_from_string("Hello, World!", content_type="text/html")
+
+print "Success! File written:", blob.public_url
